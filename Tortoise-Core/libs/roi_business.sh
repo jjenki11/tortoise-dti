8cb55ee5ca5021f4b6828a7f050c0800a78446e7
jcjenkins <jeff.jenkins.1986@gmail.com>
2015-08-27 15:23:14 -0400

    Removed unnecessary commented out code

--- Tortoise-Core/libs/roi_business.sh
+++ Tortoise-Core/libs/roi_business.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
 #ROI Bizness
-
 neda_path=/stbb_home/sadeghin/cDTIeDTI
-
 code_path=/raid1e/raid1_restore/okan/ROI_business/continuous_ROI
-
-#data_path=/stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working/
 data_path=$5
-#all_lists_filename, all_tensors_filename,ROIs_filename,outputfilename
-
-# /stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working_atlas/test_roi
-
 cd $data_path
 cd ../
 pushd $PWD
@@ -23,8 +14,7 @@ for x in $arr
 do
     echo "$x"
     find ${data_path}*_proc -name '*_'$x'.nii' ! -name 'cd*' > $data_path/tensor_${x}_files.txt
-    echo ' ' >> $data_path/tensor_${x}_files.txt
-    
+    echo ' ' >> $data_path/tensor_${x}_files.txt    
     lfn=$data_path/tensor_${x}_files.txt
     tfn=$data_path/tensor_${x}_files.txt
     rfn=$2
@@ -36,31 +26,10 @@ do
     echo 'ofn = '${ofn}
     echo 'group='${group}
     idl -e "extract_roi_values_and_save_jeff,'${lfn}','${tfn}','${rfn}','${ofn}_${x}.csv','${group}','${x}'"
-    #idl -e "extract_roi_values_and_save_jeff,'${lfn}','${tfn}','${rfn}','${ofn}.csv','${group}','${x}'"
     echo 'done with roi business for derived value: '${x}    
     cat ${ofn}_${x}.csv >> ${ofn}.csv
     rm ${ofn}_${x}.csv
 done
-
-
-
-#exit
-#lfn=$1
-#lfn='/stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working/tensor_FA_files.txt'
-#tfn=$2
-#tfn='/stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working/tensor_FA_files.txt'
-#rfn=$3
-#rfn='/stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working_atlas/roi_list.txt'
-#ofn=$4
-#ofn="/stbb_home/jenkinsjc/Desktop/tortoise-dti-master/tortoise-dti/Tortoise-Web-Services/Server/Projects/atlas_test/working_atlas/roi_business_out.csv"
-
-
-
-
-
-
-
-
 popd
 
 echo "wow, its done??"
