28fb62908ffd8ca3543c4160b839ae7768e4e35b
jjenki11 <jjenki11@gmu.edu>
2015-09-02 19:40:41 -0400

    build executable creator to be used during setup process

--- QuickStart.sh
+++ QuickStart.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 echo 'this will do an automatic check for updates then launch the server.  after this is successful, the application will launch in the browser.'
+path=$PWD
+xterm -e git pull &
 
-git pull
+xterm -e node /home/jeff/Desktop/tortoise_repo/tortoise-dti/Tortoise-Web-Services/Server/server.js &
 
-node Tortoise-Web-Services/Server/server.js &
-
-firefox Tortoise-Web-Services/Web/index_page.html &
+firefox /home/jeff/Desktop/tortoise_repo/tortoise-dti/Tortoise-Web-Services/Web/index_page.html &
---
+++ create_icon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#  This script creates an icon as well as an executable file on the desktop.
+#     Double click the turtle icon and you can minimize the server window
+
+p=$PWD
+name="QuickStart.desktop"
+exe_name="Tortoise Atlas Manager"
+explain="Run web server and client app"
+icon_path=$p/Tortoise-Web-Services/Web/img/turtle-icon.ico
+exe_path=$p/QuickStart.sh
+term="false"
+appType="Application"
+
+rm ~/Desktop/QuickStart.desktop
+echo "[Desktop Entry]" > ~/Desktop/QuickStart.desktop
+echo "Name=$exe_name" >> ~/Desktop/QuickStart.desktop
+echo "Comment=$explain" >> ~/Desktop/QuickStart.desktop
+echo "Exec=$exe_path" >> ~/Desktop/QuickStart.desktop
+echo "Icon=$icon_path" >> ~/Desktop/QuickStart.desktop
+echo "Terminal=$term" >> ~/Desktop/QuickStart.desktop
+echo "Type=$appType" >> ~/Desktop/QuickStart.desktop
+chmod 777 ~/Desktop/QuickStart.desktop
+
